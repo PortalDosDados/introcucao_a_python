34ba3f51bb26bd589ab7276da922f6756da92dd9
Dione Nascimento <morrone.dione@gmail.com>
2025-06-15 17:37:27 -0300

    Summary

---
+++ cap_02/README.md
@@ -0,0 +1,11 @@
+# Capítulo 2 ■ Preparando o ambiente
+
+Esse capítulo tem como proposito ensinar como instalar o interpretador do Python, no livro é usado a versão 3.4.
+
+Se você já sabe fazer isso, pule esse capítulo.
+
+
+![1750019276064](image/README/1750019276064.png)
+
+
+Figura 2.21 – Nomes dos elementos do primeiro programa.
---
+++ cap_02/main.py
@@ -0,0 +1,20 @@
+#%%
+# Primeiro programa com variáveis
+# Variáveis são espaços na memória do computador que guardam valores
+
+a = 2 # atribui o valor 2 à variável a
+b = 3 # atribui o valor 3 à variável b
+print(a + b) # imprime a soma de a e b
+
+
+
+
+#%%
+salario = 1500
+aumento = 0.05 # atribui o valor 0,05 à variável aumento
+print(salario * (1+aumento)) # imprime o valor do aumento
+
+
+# %%
+
+
